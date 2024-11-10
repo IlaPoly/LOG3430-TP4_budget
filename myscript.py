91b5578b0c9848ea0598346a765133e46bd9a940
IlaPoly <112783000+IlaPoly@users.noreply.github.com>
2024-11-09 19:41:00 -0500

    TP

--- myscript.py
+++ myscript.py
@@ -1,8 +1,6 @@
 import os
 os.system("git bisect start $badhash $goodhash")
-os.system("git bisect run python -m pytest")
-if result == 0:
-        # Récupérer le premier commit identifié comme mauvais
-        bad_commit = subprocess.check_output("git bisect bad", shell=True).decode().strip()
-        print(f"First bad commit: {bad_commit}")
+if os.system("git bisect run python -m pytest") == 0:
+    bad_commit = subprocess.check_output("git bisect bad", shell=True).decode().strip()
+    print(f"First bad commit: {bad_commit}")
 os.system("git bisect reset")
